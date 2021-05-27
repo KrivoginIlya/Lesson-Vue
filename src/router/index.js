637a37b7b7f9d384bf30ca3e29ab3e085789ffef
KrivoginIlya <Xline174@gmail.com>
2021-05-27 17:38:43 +0300

    lesson 1 add file

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Calc from "../views/Calc.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Calc",
+    component: Calc,
   },
   {
     path: "/about",
--- src/views/Calc.vue
+++ src/views/Calc.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Calculete msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Calculete from "@/components/Calculete.vue";
 
 export default {
-  name: "Home",
+  name: "Calc",
   components: {
-    HelloWorld,
+    Calculete,
   },
 };
 </script>
