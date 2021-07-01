e29491f2b0f164b0ac116de6bfa30b2a769d35a1
KrivoginIlya <Xline174@gmail.com>
2021-07-01 16:10:23 +0300

    add file lesson2 and new branch

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Calc</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
@@ -14,11 +14,11 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: black;
 }
 
 #nav {
-  padding: 30px;
+  padding: 32px;
 
   a {
     font-weight: bold;
--- src/views/Calc.vue
+++ src/views/Calc.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img
+      alt="Vue logo"
+      src="https://appstudio.org/wp-content/uploads/2017/10/Icons-03-uai-258x258.png"
+    />
     <Calculete msg="Welcome to Your Vue.js App" />
+    <Counter />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Calculete from "@/components/Calculete.vue";
+import Counter from "@/components/Counter.vue";
 
 export default {
   name: "Calc",
   components: {
     Calculete,
+    Counter,
   },
 };
 </script>
